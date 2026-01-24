b9ce98004e9eca23ae23b648b94dbb6aba442815
hiskome <defremov11@gmail.com>
2026-01-24 12:26:48 +0300

    debug env

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print("DEBUG BOT_TOKEN =", repr(os.getenv("BOT_TOKEN")))
+print("DEBUG ENV has BOT_TOKEN =", "BOT_TOKEN" in os.environ)
+
 TOKEN = os.getenv("BOT_TOKEN", "").strip()
 if not TOKEN:
     raise RuntimeError("Не найден BOT_TOKEN. Добавь его в .env")
